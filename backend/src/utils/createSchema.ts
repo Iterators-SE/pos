746650b6010111a71ba89450bbe93e5e24649b3c
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-03-11 00:51:44 +0800

    feat: add login and signup resolvers

--- backend/src/utils/createSchema.ts
+++ backend/src/utils/createSchema.ts
@@ -1,9 +1,11 @@
 import { buildSchema } from "type-graphql";
 import { authChecker } from "./authChecker";
 import { UserResolver } from "../resolvers/UserResolver";
+import { LoginResolver } from "../resolvers/LoginResolver";
+import { SignupResolver } from "../resolvers/SignupResolver";
 
 export const createSchema = () => buildSchema({
-    resolvers: [UserResolver],
+    resolvers: [UserResolver, LoginResolver, SignupResolver],
     authChecker,
-    authMode: "null"
+    authMode: "null",
 });
