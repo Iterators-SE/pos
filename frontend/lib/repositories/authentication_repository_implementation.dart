a0719c171a032c0cda0a04c4c024698c3af2885f
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-03-15 00:01:47 +0800

    feat: create initial authentication repository implementation

---
+++ frontend/lib/repositories/authentication_repository_implementation.dart
@@ -0,0 +1,67 @@
+import 'package:either_option/either_option.dart';
+import 'package:graphql/client.dart';
+
+import '../core/error/exception.dart';
+import '../core/error/failure.dart';
+import '../datasources/authentication/authentication_datasource.dart';
+import '../models/user.dart';
+import 'authentication_repository.dart';
+
+class AuthenticationRepository implements IAuthenticationRepository {
+  AuthenticationRepository({this.remote});
+
+  final IAuthenticationDataSource remote;
+
+  @override
+  Future<Either<Failure, User>> login({String email, String password}) async {
+    try {
+      final data = await remote.login(email: email, password: password);
+      return Right(data);
+    } on OperationException catch (e) {
+      return Left(OperationFailure(e.graphqlErrors.first.message));
+    } on NoResultsFoundException {
+      return Left(NoResultsFoundFailure());
+    } on Exception {
+      return Left(ServerFailure());
+    } catch (e) {
+      return Left(UnhandledFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, bool>> logout() async {
+    try {
+      final data = await remote.signup();
+      return Right(data);
+    } on OperationException catch (e) {
+      return Left(OperationFailure(e.graphqlErrors.first.message));
+    } on NoResultsFoundException {
+      return Left(NoResultsFoundFailure());
+    } on Exception {
+      return Left(ServerFailure());
+    } catch (e) {
+      return Left(UnhandledFailure());
+    }
+  }
+
+  @override
+  Future<Either<Failure, bool>> signup(
+      {String name, String email, String password}) async {
+    try {
+      final data = await remote.signup(
+        name: name,
+        email: email,
+        password: password,
+      );
+      return Right(data);
+    } on OperationException catch (e) {
+      return Left(OperationFailure(e.graphqlErrors.first.message));
+    } on NoResultsFoundException {
+      return Left(NoResultsFoundFailure());
+    } on Exception {
+      return Left(ServerFailure());
+    } catch (e) {
+      return Left(UnhandledFailure());
+    }
+  }
+}
