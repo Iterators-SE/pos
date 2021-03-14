3aa7db215e8260471f76be1762dfd290a64ea6c0
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-03-15 00:01:22 +0800

    feat: create initial authentication repository

---
+++ frontend/lib/repositories/authentication_repository.dart
@@ -0,0 +1,11 @@
+import 'package:either_option/either_option.dart';
+
+import '../core/error/failure.dart';
+import '../models/user.dart';
+
+abstract class IAuthenticationRepository {
+  Future<Either<Failure, bool>> signup(
+      {String name, String email, String password});
+  Future<Either<Failure, User>> login({String email, String password});
+  Future<Either<Failure, bool>> logout();
+}
