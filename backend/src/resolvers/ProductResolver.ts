241e46aea5b33beef2a66789b94501d5920f5bbf
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-03-18 17:56:07 +0800

    refactor: add authorized decorator and update ownerid to owner

--- backend/src/resolvers/ProductResolver.ts
+++ backend/src/resolvers/ProductResolver.ts
@@ -1,55 +1,62 @@
 import { Resolver, Query, Arg, Mutation, Authorized, Ctx } from "type-graphql";
 import { ChangeProductDetailsInput } from "../inputs/ChangeProductDetailsInput";
-import { ChangeUserDetailsInput } from "../inputs/ChangeUserDetailsInput";
 import { Product } from "../models/Product";
+import { User } from "../models/User";
 import { Context } from "../types/context";
 require('dotenv').config()
 
 @Resolver(of => Product)
 export class ProductResolver {
+    @Authorized()
     @Mutation(() => Boolean, { nullable: true })
 
     async addProduct(@Ctx() ctx: Context, @Arg("productname") productname: string, @Arg("description") description: string, @Arg("taxable") taxable: boolean) {
+        const user = await User.findOne({id: ctx.currentUser.id});
+
         await Product.create({
             productname,
             description,
-            ownerid: ctx.currentUser.id,
+            owner: user,
             taxable,
             photolink: "http://something/1234" // will work on this at a later time
         }).save();
 
         return true;
     }
-    
-    @Mutation(() => Boolean, {nullable: true})
-    async deleteProduct(@Arg("productId") productID: number){
-        const product = await Product.findOne({ where: { ownerid: 1, id: productID} });
-        if(!product) throw new Error("Deletion not possible! Product doesn't exist!")
+
+    @Authorized()
+    @Mutation(() => Boolean, { nullable: true })
+    async deleteProduct(@Arg("productId") productID: number) {
+        const product = await Product.findOne({ where: { owner: 1, id: productID }, relations: ["user"]  });
+        if (!product) throw new Error("Deletion not possible! Product doesn't exist!")
         await product.remove()
         return true;
     }
 
+    @Authorized()
     @Mutation(() => Product)
-        async changeProductDetails(@Ctx() ctx: Context, @Arg("productid") productid: number, @Arg("data") data: ChangeProductDetailsInput) {
-        const product = await Product.findOne({ where: { id: productid, ownerid: ctx.currentUser.id} });
+    async changeProductDetails(@Ctx() ctx: Context, @Arg("productid") productid: number, @Arg("data") data: ChangeProductDetailsInput) {
+        const product = await Product.findOne({ where: { id: productid, owner: ctx.currentUser.id } });
         if (!product) throw new Error("Can't update a non existent product!");
         Object.assign(product, data);
         await product.save();
         return product;
     }
 
-    @Query(() => Product, {nullable: true})
-    async getProducts(@Ctx() ctx: Context){
-        const products = await Product.find({ where: { ownerid: ctx.currentUser.id } });
+    @Authorized()
+    @Query(() => [Product], { nullable: true })
+    async getProducts(@Ctx() ctx: Context) {
+        const products = await Product.find({ where: { owner: ctx.currentUser.id } });
         return products;
     }
 
-    @Query(() => Product, {nullable: true})
-    async getProductDetails(@Ctx() ctx: Context, @Arg("productId") productID: number){
-        const productDetails = await Product.findOne({ where: { ownerid: ctx.currentUser.id, id: productID  } });
+    @Authorized()
+    @Query(() => Product, { nullable: true })
+    async getProductDetails(@Ctx() ctx: Context, @Arg("productId") productID: number) {
+        const productDetails = await Product.findOne({ where: { owner: ctx.currentUser.id, id: productID } });
         if (!productDetails) throw new Error("Product doesn't exist!");
+
+        console.log(productDetails)
         return productDetails;
     }
-
-
 }
