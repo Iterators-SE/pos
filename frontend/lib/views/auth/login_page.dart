883bfe8fa86bad5841e0b64154d679e2ccdf4d9a
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-03-20 03:20:05 +0800

    fix: update asset image path

--- frontend/lib/views/auth/login_page.dart
+++ frontend/lib/views/auth/login_page.dart
@@ -93,7 +93,7 @@ class _LoginPageState extends State<LoginPage> {
               child: ListView(
                 children: <Widget>[
                   SizedBox(height: 20),
-                  Image(image: AssetImage('Xpos.png')),
+                  Image(image: AssetImage('assets/Xpos.png')),
                   Padding(
                     padding: EdgeInsets.only(top: 40, bottom: 5),
                     child: Align(
