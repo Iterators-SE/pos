a5db388a5b11970b6675673a226167a75bcac020
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-04-01 23:22:51 +0800

    feat: create initial order model

---
+++ frontend/lib/models/order.dart
@@ -0,0 +1,24 @@
+import 'dart:collection';
+
+import 'product.dart';
+
+// TODO: Transform to ChangeNotifier
+class Order {
+  List<Product> _products = [];
+
+  void addProduct(Product product) => _products.add(product);
+  void editProduct(Product product) {
+    var index = _products.indexWhere((element) => element.id == product.id);
+
+    if (index != -1) {
+      _products[index] = product;
+    }
+  }
+
+  List<Product> get products => UnmodifiableListView(_products);
+
+  double get total {
+    return _products.fold(
+        0, (prev, next) => prev + next.quantity * next.basePrice);
+  }
+}
