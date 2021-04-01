2097b0bbbc3802dce3acfb251612d57cfb3e392a
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-04-02 01:42:10 +0800

    fix: address analyzer error

--- frontend/lib/presenters/orders/order_screen_presenter.dart
+++ frontend/lib/presenters/orders/order_screen_presenter.dart
@@ -7,6 +7,7 @@ import '../../views/orders/order_screen_view.dart';
 class OrderScreenPresenter extends BasePresenter<OrderScreenView> {
   // Widget getMessage() {
   //   checkViewAttached();
-  //   return isViewAttached  && getView().hasProducts ? getView().body : getView().emptyOrder;
+  //   return isViewAttached  && getView().hasProducts ? 
+  // getView().body : getView().emptyOrder;
   // }
 }
