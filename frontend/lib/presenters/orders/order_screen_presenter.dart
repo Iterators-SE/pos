147f5c6812067b2b6b0c471928f46bbbc62053a8
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-04-02 01:04:51 +0800

    feat: create order screen presenter

---
+++ frontend/lib/presenters/orders/order_screen_presenter.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+import '../../core/ui/base_presenter.dart';
+import 'order_screen_view.dart';
+
+class OrderScreenPresenter extends BasePresenter<OrderScreenView> {
+  // Widget getMessage() {
+  //   checkViewAttached();
+  //   return isViewAttached  && getView().hasProducts ? getView().body : getView().emptyOrder;
+  // }
+}
