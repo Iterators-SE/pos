ded420188fc30296d53eeb3bee37be555dcb5b4e
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-04-03 00:09:30 +0800

    feat: add product variant model

---
+++ frontend/lib/models/product_variant.dart
@@ -0,0 +1,8 @@
+class ProductVariant {
+  final int id;
+  final double basePrice;
+  final int quantity;
+  final String variant;
+
+  ProductVariant({this.id, this.basePrice, this.quantity, this.variant});
+}
