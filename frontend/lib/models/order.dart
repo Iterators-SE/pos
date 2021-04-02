19282c69898208d9d8f358c8ce0c8d47ae9a5a15
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-04-03 00:11:34 +0800

    refactor: update order model

--- frontend/lib/models/order.dart
+++ frontend/lib/models/order.dart
@@ -1,13 +1,13 @@
 import 'dart:collection';
 
-import 'product.dart';
+import 'product_variant.dart';
 
 // TODO: Transform to ChangeNotifier
 class Order {
-  final List<Product> _products = [];
+  final List<ProductVariant> _products = [];
 
-  void addProduct(Product product) => _products.add(product);
-  void editProduct(Product product) {
+  void addProduct(ProductVariant product) => _products.add(product);
+  void editProduct(ProductVariant product) {
     var index = _products.indexWhere((element) => element.id == product.id);
 
     if (index != -1) {
@@ -15,7 +15,7 @@ class Order {
     }
   }
 
-  List<Product> get products => UnmodifiableListView(_products);
+  List<ProductVariant> get products => UnmodifiableListView(_products);
 
   double get total {
     return _products.fold(
