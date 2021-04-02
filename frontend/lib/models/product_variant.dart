ec3b8048491c96b79d8387772e14c550f9d82ec2
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-04-03 00:22:51 +0800

    refactor: add parent instance variable to producr variant model

--- frontend/lib/models/product_variant.dart
+++ frontend/lib/models/product_variant.dart
@@ -1,8 +1,15 @@
 class ProductVariant {
   final int id;
+  final String parent;
   final double basePrice;
   final int quantity;
   final String variant;
 
-  ProductVariant({this.id, this.basePrice, this.quantity, this.variant});
+  ProductVariant({
+    this.id,
+    this.parent,
+    this.basePrice,
+    this.quantity,
+    this.variant,
+  });
 }
