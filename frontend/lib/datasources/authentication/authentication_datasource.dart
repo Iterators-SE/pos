ddcf99e03b44f2953d7178e17d1deba7ee687c3a
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-04-05 00:39:13 +0800

    refactor: change logout return from bool to void

--- frontend/lib/datasources/authentication/authentication_datasource.dart
+++ frontend/lib/datasources/authentication/authentication_datasource.dart
@@ -1,8 +1,10 @@
+import 'package:flutter/material.dart';
+
 import '../../models/user.dart';
 
 abstract class IAuthenticationDataSource {
   Future<bool> signup(
       {String name, String email, String password});
   Future<User> login({String email, String password});
-  Future<bool> logout();
+  Future<void> logout();
 }
