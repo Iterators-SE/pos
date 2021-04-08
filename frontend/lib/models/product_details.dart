a54222f8e5665958aa271c12eccf1c5463af5e2e
Alanray Elangos <ealanray@gmail.com>
2021-04-08 14:56:23 +0800

    feat: added getters

--- frontend/lib/models/product_details.dart
+++ frontend/lib/models/product_details.dart
@@ -1,9 +1,20 @@
 class ProductDetails {
+  final int productId;
   final String productName;
   final String description;
   final bool taxable;
   final String photoLink;
 
   ProductDetails(
-      {this.productName, this.description, this.taxable, this.photoLink});
+      {this.productId,
+      this.productName,
+      this.description,
+      this.taxable,
+      this.photoLink});
+
+  int get id => productId;
+  String get name => productName;
+  String get desc => description;
+  bool get isTaxable => taxable;
+  String get photo => photoLink;
 }
