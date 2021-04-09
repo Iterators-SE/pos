446be719051d2ec8c92e3d87c76729f40f4416f7
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-04-09 15:20:38 +0800

    refactor: update signup ui

--- frontend/lib/features/authentication/screens/widgets/signup_view.dart
+++ frontend/lib/features/authentication/screens/widgets/signup_view.dart
@@ -22,85 +22,92 @@ class _SignupWidgetState extends State<SignupWidget> {
       padding: EdgeInsets.only(top: 80, left: 40, right: 40),
       child: Form(
         key: widget.formKey,
-        child: SizedBox(
-          width: 400,
-          child: ListView(
-            children: <Widget>[
-              Spacer(),
-              Image(image: AssetImage('assets/images/Xpos.png')),
-              Padding(
-                padding: EdgeInsets.only(top: 40, bottom: 5),
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    "Signup",
-                    style: TextStyle(
-                      fontSize: 20,
-                      color: Colors.black,
-                    ),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Spacer(),
+            Container(
+              constraints: BoxConstraints(maxHeight: 150),
+              child: Image(
+                image: AssetImage('assets/images/Xpos.png'),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 40, bottom: 5),
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Signup",
+                  style: TextStyle(
+                    fontSize: 20,
+                    color: Colors.black,
                   ),
                 ),
               ),
-              Padding(
-                padding: EdgeInsets.only(
-                  bottom: 30,
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                bottom: 30,
+              ),
+              child: Align(
+                alignment: Alignment.topLeft,
+                child: Text(
+                  "Create your store",
+                  style: TextStyle(fontSize: 12, color: Colors.black),
                 ),
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Text(
-                    "Create your store",
-                    style: TextStyle(fontSize: 12, color: Colors.black),
-                  ),
-                ),
               ),
-              TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    hintText: 'Enter Email',
-                    labelText: 'Email'),
-                validator: (value) => value.isEmpty
-                    ? 'Email can\'t be empty'
-                    : !EmailValidator.validate(value.toString())
-                        ? "Email Is Invalid"
-                        : null,
-                onChanged: (value) => setState(() => email = value),
-              ),
-              Spacer(),
-              TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    hintText: 'Enter Password',
-                    labelText: 'Password'),
-                obscureText: true,
-                validator: (value) => value.isEmpty
-                    ? "Password can\'t be empty"
-                    : value.trim().length <= 8
-                        ? "Password must be at least 8 characters long"
-                        : null,
-                onChanged: (value) => setState(() => password = value),
-              ),
-              Spacer(),
-              TextFormField(
-                decoration: InputDecoration(
-                    border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10)),
-                    hintText: 'Enter store name',
-                    labelText: 'Store Name'),
-                validator: (value) =>
-                    value.isEmpty ? "Store name can\'t be empty" : null,
-                onChanged: (value) => setState(() => name = value),
-              ),
-              Spacer(),
-              Container(
-                height: 40,
-                width: 80,
-                decoration: BoxDecoration(
-                    color: Colors.green[900],
-                    borderRadius: BorderRadius.circular(10)),
-                child: MaterialButton(
-                  onPressed: () {
+            ),
+            TextFormField(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  hintText: 'Enter Email',
+                  labelText: 'Email'),
+              validator: (value) => value.isEmpty
+                  ? 'Email can\'t be empty'
+                  : !EmailValidator.validate(value.toString())
+                      ? "Email Is Invalid"
+                      : null,
+              onChanged: (value) => setState(() => email = value),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              autovalidateMode: AutovalidateMode.onUserInteraction,
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  hintText: 'Enter Password',
+                  labelText: 'Password'),
+              obscureText: true,
+              validator: (value) => value.isEmpty
+                  ? "Password can\'t be empty"
+                  : value.trim().length <= 8
+                      ? "Password must be at least 8 characters long"
+                      : null,
+              onChanged: (value) => setState(() => password = value),
+            ),
+            SizedBox(height: 20),
+            TextFormField(
+              decoration: InputDecoration(
+                  border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(10)),
+                  hintText: 'Enter store name',
+                  labelText: 'Store Name'),
+              validator: (value) =>
+                  value.isEmpty ? "Store name can\'t be empty" : null,
+              onChanged: (value) => setState(() => name = value),
+            ),
+            Spacer(),
+            Container(
+              height: 40,
+              width: 80,
+              decoration: BoxDecoration(
+                  color: Colors.green[900],
+                  borderRadius: BorderRadius.circular(10)),
+              child: MaterialButton(
+                onPressed: () {
                   widget.formKey.currentState.validate()
                       ? widget.signup(
                           context,
@@ -110,24 +117,27 @@ class _SignupWidgetState extends State<SignupWidget> {
                         )
                       : null;
                 },
-                  child: Text(
-                    'Signup',
-                    style: TextStyle(color: Colors.white),
-                  ),
+                child: Text(
+                  'Signup',
+                  style: TextStyle(color: Colors.white),
                 ),
               ),
-              TextButton(
-                style: TextButton.styleFrom(
-                    textStyle: TextStyle(color: Colors.grey)),
-                child: Text(
-                  'Already have an account? Login Here!',
-                  textAlign: TextAlign.end,
+            ),
+            Align(
+              alignment: Alignment.centerRight,
+              child: MaterialButton(
+                child: RichText(
+                  text: TextSpan(
+                    text: 'Already have an account? ',
+                    style: TextStyle(color: Colors.grey[500], fontSize: 12),
+                    children: [TextSpan(text: 'Login!')],
+                  ),
                 ),
                 onPressed: widget.toggle,
               ),
-              Spacer()
-            ],
-          ),
+            ),
+            Spacer()
+          ],
         ),
       ),
     );
