8b29168a2dd398f441a690ceb0ebeaa25b0d3c21
Alanray Elangos <ealanray@gmail.com>
2021-04-27 19:31:38 +0800

    feat: added a graphql service that is subject to change

---
+++ frontend/lib/graphql/graphql_config.dart
@@ -0,0 +1,37 @@
+import "package:flutter/material.dart";
+import "package:graphql_flutter/graphql_flutter.dart";
+
+class GraphQLConfiguration {
+  static Link link;
+  static HttpLink httpLink = HttpLink("http://localhost:5000/graphql");
+
+  static void setToken(String token) {
+    var alink = AuthLink(getToken: () async => 'Bearer $token');
+    GraphQLConfiguration.link = alink.concat(GraphQLConfiguration.httpLink);
+  }
+
+  static void removeToken() {
+    GraphQLConfiguration.link = null;
+  }
+
+  static Link getLink() {
+    return GraphQLConfiguration.link != null
+        ? GraphQLConfiguration.link
+        : GraphQLConfiguration.httpLink;
+  }
+  
+  GraphQLClient clientToQuery() {
+    return GraphQLClient(
+      cache: GraphQLCache(),
+      link: getLink(),
+    );
+  }
+
+  ValueNotifier<GraphQLClient> client = ValueNotifier(
+    GraphQLClient(
+      link: getLink(),
+      cache: GraphQLCache(),
+    ),
+  );
+
+}
