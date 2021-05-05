e8dd4552720f44c65b742b16ec4bab4b2fec1a0c
Alanray Elangos <ealanray@gmail.com>
2021-05-05 23:36:48 +0800

    feat: added firebase core initializer

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:graphql/client.dart';
@@ -15,8 +16,9 @@ import 'providers/user_provider.dart';
 import 'repositories/authentication/authentication_repository.dart';
 import 'repositories/authentication/authentication_repository_implementation.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+   await Firebase.initializeApp();
 
   HttpLink _httpLink;
   GraphQLClient _client;
