0ddf1e441f49669180edad5b793710cc9b028536
Majpotatooo <majella.jereza.phowzzz@gmail.com>
2021-05-06 17:21:57 +0800

    feat: product search

---
+++ frontend/lib/views/search products/constants.dart
@@ -0,0 +1,65 @@
+
+
+const coffeeData = [
+  {
+    "name": "Frappuccino",
+    "description": "A blended iced coffee drink that is topped with whipped cream and syrup",
+    "stocks": "18",
+  },
+  {
+    "name": "Iced Coffee",
+    "description": "A coffee with ice, typically served with a dash of milk, cream or sweetener",
+    "stocks": "13"
+  },
+  {
+    "name": "Latte",
+    "description": "Comprised of a shot of espresso and steamed milk with just a touch of foam",
+    "stocks": "8"
+  },
+  {
+    "name": "Cappuccino",
+    "description": "A latte made with more foam than steamed milkwith a sprinkle of cocoa powder top",
+    "stocks": "31"
+  },
+  {
+    "name": "Espresso",
+    "description": "This can be served solo or used as the foundation of most coffee drinks",
+    "stocks": "21"
+  },
+  {
+    "name": "Macchiato",
+    "description": "Another espresso-based drink that has a small amount of foam on top",
+    "stocks": "17"
+  },
+  {
+    "name": "Mocha",
+    "description": "chocolate espresso drink with steamed milk and foam",
+    "stocks": "12"
+  },
+  {
+    "name": "Flat White",
+    "description": "A coffee drink with a double espresso and lightly frosted milk",
+    "stocks": "6"
+  },
+  {
+    "name": "Frappe",
+    "description": "Rich iced coffee made of espresso, milk and ice with flavoured syrup added",
+    "stocks": "28"
+  },
+  {
+    "name": "Iced Mocha",
+    "description": "Prepared like iced latte, but garnished and flavoured like caffè mocha",
+    "stocks": "9"
+  },
+  {
+    "name": "Affogato",
+    "description": "Made by pouring a shot of espresso over a scoop of vanilla ice cream",
+    "stocks": "3"
+  },
+  {
+    "name": "Cortado",
+    "description": "Takes the macchiato one step further by evenly balancing the espresso with warm milk in order to reduce the acidity",
+    "stocks": "17"
+  },
+  
+];
