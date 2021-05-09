e4808cb86cc0d8563252e91993e8ab6079aa0bc3
Alanray Elangos <ealanray@gmail.com>
2021-05-09 17:17:03 +0800

    feat: added a new look for product details

--- frontend/lib/features/inventory/details/product_details.dart
+++ frontend/lib/features/inventory/details/product_details.dart
@@ -16,37 +16,36 @@ class ProductDetails extends StatefulWidget {
 }
 
 class _ProductDetailsState extends State<ProductDetails> {
-  dynamic getVariants() async {
+  dynamic getProductDetails() async {
     var query = MutationQuery();
     var client = GraphQLConfiguration().clientToQuery();
-
-    print(widget.productData['id']);
-
-    var result = await client.query(QueryOptions(
-        document: gql(query.getVariants(int.parse(widget.productData['id'])))));
-
-    print(result.data);
-
-    return result.data['getVariants'];
   }
 
   void deleteProduct() async {
     var query = MutationQuery();
     var client = GraphQLConfiguration().clientToQuery();
+    var deleteAllVariantsResult = await client.mutate(
+      MutationOptions(
+        document: gql(
+          query.deleteAllVariants(
+            int.parse(widget.productData['product']['id']),
+          ),
+        ),
+      ),
+    );
 
-    var deleteAllVariantsResult = await client.mutate(MutationOptions(
-        document:
-            gql(query.deleteAllVariants(int.parse(widget.productData['id'])))));
-
-    print(deleteAllVariantsResult);
     if (deleteAllVariantsResult.data['deleteAllVariants'] != true) {
-      print("dis  ran!");
       return null;
     }
-
-    var deleteProductResult = await client.mutate(MutationOptions(
-        document:
-            gql(query.deleteProduct(int.parse(widget.productData['id'])))));
+    var deleteProductResult = await client.mutate(
+      MutationOptions(
+        document: gql(
+          query.deleteProduct(
+            int.parse(widget.productData['product']['id']),
+          ),
+        ),
+      ),
+    );
 
     if (deleteProductResult.data['deleteProduct']) {
       Navigator.pushAndRemoveUntil(
@@ -56,17 +55,102 @@ class _ProductDetailsState extends State<ProductDetails> {
     }
   }
 
+  Widget isTaxable(bool taxable) {
+    if (taxable) {
+      return Text(
+        "Taxes apply to this product",
+        style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
+      );
+    }
+    return Text(
+      "Taxes do not apply to this product",
+      style: TextStyle(fontSize: 15, fontWeight: FontWeight.normal),
+    );
+  }
+
   void _editProductDetails() async {
-    Navigator.pushAndRemoveUntil(
-        context,
-        MaterialPageRoute(
-            builder: (context) => EditDetails(productData: widget.productData)),
-        (route) => false);
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) =>
+            EditDetails(productData: widget.productData['product']),
+      ),
+    );
+  }
+
+  dynamic showVariant() {
+    List<Widget> widgets = [];
+
+    for (var i = 0; i < widget.productData['variants'].length; i++) {
+      widgets.add(
+        variantBox(
+          widget.productData['variants'][i]['variantname'],
+          widget.productData['variants'][i]['quantity'],
+          widget.productData['variants'][i]['price'],
+        ),
+      );
+    }
+
+    return Container(
+      width: 350,
+      child: Column(children: widgets),
+    );
+  }
+
+  Widget variantBox(String name, int quantity, int price) {
+    return Column(
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: <Widget>[
+            Flexible(
+              child: TextFormField(
+                readOnly: true,
+                initialValue: '$name',
+                decoration: InputDecoration(
+                    labelText: 'Name',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    )),
+              ),
+            ),
+            SizedBox(
+              width: 15.0,
+            ),
+            Flexible(
+              child: TextFormField(
+                readOnly: true,
+                initialValue: '$quantity',
+                decoration: InputDecoration(
+                    labelText: 'Quantity',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    )),
+              ),
+            ),
+            SizedBox(
+              width: 15.0,
+            ),
+            Flexible(
+              child: TextFormField(
+                readOnly: true,
+                initialValue: '$price',
+                decoration: InputDecoration(
+                    labelText: 'Price',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(30),
+                    )),
+              ),
+            ),
+          ],
+        ),
+        SizedBox(height: 10),
+      ],
+    );
   }
 
   @override
   Widget build(BuildContext context) {
-    getVariants();
     return Scaffold(
       appBar: AppBar(
         title: Text("Product Details"),
@@ -89,84 +173,110 @@ class _ProductDetailsState extends State<ProductDetails> {
               }),
         ],
       ),
-      body: Container(
-          child: Column(
-        children: [
-          Container(
-            height: 500,
-            child: ListView(
-              children: [
-                Image.network(widget.productData['photolink']),
-                Padding(
-                  padding: EdgeInsets.only(top: 25),
-                  child: Text(
-                    widget.productData['productname'],
-                    style: TextStyle(
-                      fontSize: 50.0,
-                      fontWeight: FontWeight.bold,
-                    ),
-                    textAlign: TextAlign.center,
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.network(widget.productData['product']['photolink']),
+            SizedBox(height: 25),
+            Container(
+              width: 350,
+              child: TextFormField(
+                style: TextStyle(fontSize: 25, fontWeight: FontWeight.bold),
+                initialValue: widget.productData['product']['productname'],
+                decoration: InputDecoration(
+                  labelText: 'Product Name',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
                   ),
                 ),
-                SizedBox(
-                  height: 15.0,
-                ),
-                Text(
-                  widget.productData['description'],
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 25.0,
-                    fontWeight: FontWeight.w100,
-                  ),
-                ),
-                SizedBox(
-                  height: 10.0,
-                ),
-                Text(
-                  "Taxable: ${widget.productData['taxable']}",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: 20.0,
-                    fontWeight: FontWeight.w100,
-                  ),
-                ),
-                // _variantWidget(10),
-              ],
+              ),
             ),
-          ),
-          Container(
-            height: 250,
-            width: 350,
-            child: FutureBuilder(
-                future: getVariants(),
-                builder: (context, snapshot) {
-                  if (snapshot.data == null) {
-                    return Container(child: Center(child: Text("Loading")));
-                  } else {
-                    return ListView.builder(
-                        itemCount: snapshot.data.length,
-                        itemBuilder: (context, index) {
-                          print(snapshot.data.length);
-                          print(snapshot.data[index]);
-                          return Card(
-                            margin: EdgeInsets.all(10),
-                            child: Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                              children: [
-                                // ignore: lines_longer_than_80_chars
-                                Text("Variant Name: ${snapshot.data[index]['variantname']}"),
-                                Text("Price: ${snapshot.data[index]['price']}"),
-                                // ignore: lines_longer_than_80_chars
-                                Text("Quantity: ${snapshot.data[index]['quantity']}"),
-                              ],
-                            ),
-                          );
-                        });
-                  }
-                }),
-          )
+            SizedBox(height: 25),
+            Container(
+              width: 350,
+              child: TextFormField(
+                minLines: 1,
+                maxLines: 5,
+                style: TextStyle(fontSize: 15, fontWeight: FontWeight.bold),
+                initialValue: widget.productData['product']['description'],
+                decoration: InputDecoration(
+                  labelText: 'Product Description',
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(30),
+                  ),
+                ),
+              ),
+            ),
+            SizedBox(height: 10),
+            isTaxable(widget.productData['product']['taxable']),
+            SizedBox(height: 20),
+            Text(
+              "Variants",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            ),
+            SizedBox(height: 10),
+            showVariant(),
         ],
-      )),
+        ),
+      ) 
+    
+
+      // Container(
+      //     child: Column(
+      //   children: [
+      //     Container(
+      //       height: 500,
+      //       child: Column(
+      //         children: [
+      //           Image.network(widget.productData['photolink']),
+      //           Padding(
+      //             padding: EdgeInsets.only(top: 25),
+      //             child: Text(
+      //               widget.productData['productname'],
+      //               style: TextStyle(
+      //                 fontSize: 50.0,
+      //                 fontWeight: FontWeight.bold,
+      //               ),
+      //               textAlign: TextAlign.center,
+      //             ),
+      //           ),
+      //           SizedBox(
+      //             height: 15.0,
+      //           ),
+      //           Text(
+      //             widget.productData['description'],
+      //             textAlign: TextAlign.center,
+      //             style: TextStyle(
+      //               fontSize: 25.0,
+      //               fontWeight: FontWeight.w100,
+      //             ),
+      //           ),
+      //           SizedBox(
+      //             height: 10.0,
+      //           ),
+      //           Text(
+      //             "Taxable: ${widget.productData['taxable']}",
+      //             textAlign: TextAlign.center,
+      //             style: TextStyle(
+      //               fontSize: 20.0,
+      //               fontWeight: FontWeight.w100,
+      //             ),
+      //           ),
+      //           // _variantWidget(10),
+      //         ],
+      //       ),
+      //     ),
+      //     // Container(
+      //     //   height: 250,
+      //     //   width: 350,
+      //     //   child: Column(
+      //     //     children:
+
+      //     //   )
+
+      //     // )
+      //   ],
+      // )),
     );
   }
 }
