d1c62bf108adf3d0c2506bf43207308561d73d80
Alanray Baltazar Elangos <ealanray@gmail.com>
2021-05-19 14:37:34 +0800

    chore: removed unused import;

--- frontend/lib/datasources/inventory/product/inventory_remote_datasource.dart
+++ frontend/lib/datasources/inventory/product/inventory_remote_datasource.dart
@@ -1,7 +1,7 @@
 import 'dart:convert';
 
 import 'package:graphql/client.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+// import 'package:shared_preferences/shared_preferences.dart';
 
 import '../../../graphql/graphql_config.dart';
 import '../../../graphql/queries.dart';
