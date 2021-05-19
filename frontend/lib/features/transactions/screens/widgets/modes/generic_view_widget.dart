56b14ff0b3f6e0b493e71425ddeda4f82500cf60
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-05-19 15:51:40 +0800

    feat: add initial generic view widget

---
+++ frontend/lib/features/transactions/screens/widgets/modes/generic_view_widget.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/models/product.dart';
+import 'package:graphic/graphic.dart';
+
+class GenericViewWidget extends StatelessWidget {
+  final List<Product> products;
+
+  const GenericViewWidget({Key key, this.products}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      // TODO: Make resizeable? or scrollable?
+      // Idea: Make horizontal list view with one element?
+      height: 200,
+      width: 400,
+      child: Chart(
+        // data: [
+        //   {'genre': 'Sports', 'sold': 275},
+        //   {'genre': 'Strategy', 'sold': 115},
+        //   {'genre': 'Action', 'sold': 120},
+        //   {'genre': 'Shooter', 'sold': 350},
+        //   {'genre': 'Other', 'sold': 150},
+        // ],
+        data: products
+            .map((e) => {'product': e.name, 'sold': e.quantity})
+            .toList(),
+        scales: {
+          'product': CatScale(
+            accessor: (map) => map['product'] as String,
+          ),
+          // 'genre': CatScale(
+          //   accessor: (map) => map['genre'] as String,
+          // ),
+          'sold': LinearScale(
+            accessor: (map) => map['sold'] as num,
+            nice: true,
+          )
+        },
+        geoms: [
+          IntervalGeom(
+            position: PositionAttr(field: 'product*sold'),
+            // position: PositionAttr(field: 'genre*sold'),
+          )
+        ],
+        axes: {
+          // 'genre': Defaults.horizontalAxis,
+          'product': Defaults.horizontalAxis,
+          'sold': Defaults.verticalAxis,
+        },
+      ),
+    );
+  }
+}
