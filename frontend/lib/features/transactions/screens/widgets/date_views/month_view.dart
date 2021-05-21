a470b8ed0c9ef66f876952a0d74c02c037e3b601
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-05-21 18:56:52 +0800

    refactor: add product and top three variables to view widgets

--- frontend/lib/features/transactions/screens/widgets/date_views/month_view.dart
+++ frontend/lib/features/transactions/screens/widgets/date_views/month_view.dart
@@ -1,37 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
 
 import '../../../../../models/product.dart';
-import '../../../../../models/product_variant.dart';
 import '../modes/mode_view.dart';
 import '../top_product/top_products_widget.dart';
 
 class MonthViewWidget extends StatelessWidget {
-  final fakeProducts = [
-    Product(id: 1, name: 'Olympian Cappucino', variants: [
-      ProductVariant(
-          variantId: 1,
-          variantName: 'Small',
-          quantity: 10,
-          productId: 1,
-          price: 100)
-    ]),
-    Product(id: 2, name: 'Kapa-Kappucino', variants: [
-      ProductVariant(
-          variantId: 2,
-          variantName: 'Small',
-          quantity: 50,
-          productId: 1,
-          price: 100)
-    ]),
-    Product(id: 3, name: 'Donut', variants: [
-      ProductVariant(
-          variantId: 3,
-          variantName: 'Regular',
-          quantity: 100,
-          productId: 1,
-          price: 100)
-    ])
-  ];
+  final List<Product> products;
+  final List<Product> topThree;
+
+  const MonthViewWidget({@required this.products, @required this.topThree});
 
   @override
   Widget build(BuildContext context) {
@@ -39,9 +17,9 @@ class MonthViewWidget extends StatelessWidget {
         return Column(
         children: [
           Expanded(
-            child: TopProductsWidget(products: fakeProducts),
+            child: TopProductsWidget(products: topThree),
           ),
-          ModeView(products: fakeProducts),
+          ModeView(products: products),
         ],
       
     );
