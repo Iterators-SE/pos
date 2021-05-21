8758b3670c0b6f756cb429eeffdc42394bbe5cb4
galgo-k <62338911+galgo-k@users.noreply.github.com>
2021-05-21 15:58:02 +0800

    feat(lib\features\discount): added view discounts and discount details

--- frontend/lib/features/discount/details/discount_details.dart
+++ frontend/lib/features/discount/details/discount_details.dart
@@ -1,18 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:graphql/client.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 import "../../../core/themes/config.dart";
-import '../edit/discount_edit.dart';
+import '../../../graphql/graphql_config.dart';
+import '../../../graphql/queries.dart';
+import '../discount_page/discountpage.dart';
+import '../edit/discount_edit_generic.dart';
 import '../reusable_widgets/duration.dart';
 import '../reusable_widgets/duration_container.dart';
 import '../reusable_widgets/subtitle.dart';
 import '../reusable_widgets/title.dart';
 
 class DiscountDetails extends StatefulWidget {
+  final int id;
+  DiscountDetails({this.id});
   @override
   _DiscountDetailsState createState() => _DiscountDetailsState();
 }
 
 class _DiscountDetailsState extends State<DiscountDetails> {
+  dynamic getDiscount() async {
+    var prefs = await SharedPreferences.getInstance();
+    var token = prefs.getString('POS_TOKEN');
+    GraphQLConfiguration.setToken(token);
+    var query = MutationQuery();
+    var client = GraphQLConfiguration().clientToQuery();
+
+    var result = await client.query(
+      QueryOptions(document: gql(query.getDiscount(widget.id))),
+    );
+    print(result);
+    return result.data['getDiscount'];
+  }
+
+  void deleteDiscount() async {
+    var prefs = await SharedPreferences.getInstance();
+    var token = prefs.getString('POS_TOKEN');
+    GraphQLConfiguration.setToken(token);
+    var query = MutationQuery();
+    var client = GraphQLConfiguration().clientToQuery();
+
+    var result = await client.query(
+      QueryOptions(document: gql(query.deleteDiscount(widget.id))),
+    );
+    print(result);
+    if (!result.hasException) {
+      Navigator.pop(context);
+      Navigator.pushReplacement(
+          context, MaterialPageRoute(builder: (context) => DiscountPage()));
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,34 +60,51 @@ class _DiscountDetailsState extends State<DiscountDetails> {
         "Discounts",
         style: TextStyle(fontFamily: "Montserrat Bold"),
       )),
-      body: ListView(
-        padding: EdgeInsets.only(left: 10, right: 20),
-        children: <Widget>[
-          SizedBox(
-            height: 30,
-          ),
-          title("PWD DISCOUNT"),
-          subtitle("Product:"),
-          details("KEYK"),
-          subtitle("Promo Duration:"),
-          Container(
-              padding: EdgeInsets.all(20),
-              decoration: BoxDecoration(
-                  borderRadius: BorderRadius.all(Radius.circular(10)),
-                  border: Border.all(color: xposGreen[500])),
-              child: duration()),
-          subtitle("Discount Percent:"),
-          durationContainer("40%")
-        ],
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        icon: Icon(Icons.edit),
-        label: Text("EDIT", style: TextStyle(fontFamily: "Montserrat Bold")),
-        onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => EditDiscount()));
-        },
-      ),
+      body: FutureBuilder(
+          future: getDiscount(),
+          builder: (context, snapshot) {
+            if (snapshot.data != null) {
+              print(snapshot.data['description']);
+              return ListView(
+                padding: EdgeInsets.only(left: 10, right: 20),
+                children: <Widget>[
+                  SizedBox(
+                    height: 30,
+                  ),
+                  title(snapshot.data['description']),
+                  subtitle("Product:"),
+                  details("KEYK"),
+                  subtitle("Promo Duration:"),
+                  Container(
+                      padding: EdgeInsets.all(20),
+                      decoration: BoxDecoration(
+                          borderRadius: BorderRadius.all(Radius.circular(10)),
+                          border: Border.all(color: xposGreen[500])),
+                      child: duration()),
+                  subtitle("Discount Percent:"),
+                  durationContainer("${snapshot.data["percentage"]}%")
+                ],
+              );
+            }
+            return Text('Discount is not available');
+          }),
+      persistentFooterButtons: [
+        FloatingActionButton.extended(
+          icon: Icon(Icons.edit),
+          label: Text("EDIT", style: TextStyle(fontFamily: "Montserrat Bold")),
+          onPressed: () {
+            Navigator.push(
+                context,
+                MaterialPageRoute(
+                    builder: (context) => EditGenericDiscount(id: widget.id)));
+          },
+        ),
+        FloatingActionButton.extended(
+            icon: Icon(Icons.edit),
+            label:
+                Text("DELETE", style: TextStyle(fontFamily: "Montserrat Bold")),
+            onPressed: deleteDiscount),
+      ],
     );
   }
 }
--- frontend/lib/features/discount/discount_page/discountpage.dart
+++ frontend/lib/features/discount/discount_page/discountpage.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
-import '../../../models/mock_discounts.dart';
+import 'package:graphql_flutter/graphql_flutter.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../../graphql/graphql_config.dart';
+import '../../../graphql/queries.dart';
+import '../add/discount_add_generic.dart';
 import '../details/discount_details.dart';
-import '../edit/discount_edit.dart';
-// import '../reusable_widgets/title.dart';
+import '../reusable_widgets/title.dart';
 
 class DiscountPage extends StatefulWidget {
   @override
@@ -10,13 +14,20 @@ class DiscountPage extends StatefulWidget {
 }
 
 class _DiscountPageState extends State<DiscountPage> {
-  List<Discount> products = [
-    Discount(name: "VIP discount", percent: 50),
-    Discount(name: "PWD discount", percent: 25),
-    Discount(name: "PWD discount", percent: 25),
-    Discount(name: "PWD discount", percent: 25),
-  ];
-  List<int> colorCodes = [700, 600, 400, 300, 50];
+  // DiscountRepository _discountRepository;
+
+  dynamic getDiscounts() async {
+    var prefs = await SharedPreferences.getInstance();
+    var token = prefs.getString('POS_TOKEN');
+    GraphQLConfiguration.setToken(token);
+    var query = MutationQuery();
+    var client = GraphQLConfiguration().clientToQuery();
+
+    var result = await client.query(
+      QueryOptions(document: gql(query.getDiscounts())),
+    );
+    return result.data['getDiscounts'];
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -31,53 +42,52 @@ class _DiscountPageState extends State<DiscountPage> {
         icon: Icon(Icons.add),
         label: Text("ADD", style: TextStyle(fontFamily: "Montserrat Bold")),
         onPressed: () {
-          Navigator.push(
-              context, MaterialPageRoute(builder: (context) => EditDiscount()));
+          Navigator.push(context,
+              MaterialPageRoute(builder: (context) => AddGenericDiscount()));
         },
       ),
-      body: Padding(
-          padding: const EdgeInsets.only(left: 50, right: 50, top: 50),
-          child: ListView.separated(
-              separatorBuilder: (context, index) => const Divider(),
-              itemCount: products.length,
-              itemBuilder: (context, index) {
-                return InkWell(
-                    child: Container(
-                      padding: EdgeInsets.fromLTRB(15, 35, 15, 25),
-                      height: 100,
-                      child: Row(
-                        children: [
-                          Expanded(
-                            flex: 5,
-                            child: Text(
-                              products[index].name,
-                              style: TextStyle(
-                                  fontFamily: "Montserrat Bold", fontSize: 25),
+      body: FutureBuilder(
+          future: getDiscounts(),
+          builder: (context, snapshot) {
+            if (snapshot.data != null) {
+              return Padding(
+                padding: const EdgeInsets.only(left: 50, right: 50),
+                child: GridView.builder(
+                    itemCount: snapshot.data.length,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: MediaQuery.of(context).orientation ==
+                              Orientation.landscape
+                          ? 3
+                          : 2,
+                      crossAxisSpacing: 8,
+                      mainAxisSpacing: 8,
+                    ),
+                    itemBuilder: (context, index) {
+                      return Padding(
+                        padding: EdgeInsets.all(20),
+                        child: ElevatedButton(
+                          style: ElevatedButton.styleFrom(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30.0),
                             ),
                           ),
-                          Expanded(
-                            flex: 1,
-                            child: Text(
-                              "${products[index].percent.toString()} %",
-                              style: TextStyle(
-                                  fontFamily: "Montserrat Bold", fontSize: 25),
-                            ),
-                          )
-                        ],
-                      ),
-                      decoration: BoxDecoration(
-                          borderRadius: BorderRadius.circular(10),
-                          color: Colors.blueGrey[colorCodes[index]]),
-                      // decoration:
-                      // BoxDecoration(borderRadius: BorderRadius.circular(10)),
-                    ),
-                    onTap: () {
-                      Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                              builder: (contex) => DiscountDetails()));
-                    });
-              })),
+                          onPressed: () {
+                            Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                    builder: (context) => DiscountDetails(
+                                        id: int.parse(
+                                            snapshot.data[index]['id']))));
+                          },
+                          child: discountTitles(
+                              snapshot.data[index]['description']),
+                        ),
+                      );
+                    }),
+              );
+            }
+            return Text('No discounts found');
+          }),
     );
   }
 }
