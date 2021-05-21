ce82c28a5ecaab4e4e15510dc31bf390cfb28e5e
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-05-21 18:53:49 +0800

    refactor: add product lists, add view helpers, and remove unused fns

--- frontend/lib/features/transactions/views/transaction_screen_view.dart
+++ frontend/lib/features/transactions/views/transaction_screen_view.dart
@@ -1,13 +1,35 @@
 import 'package:flutter/material.dart';
+
+import '../../../core/error/failure.dart';
+import '../../../models/product.dart';
+import '../../../models/transaction.dart';
 import '../../../repositories/transactions/interval.dart' as interval_i;
+
+enum LoadingState { loading, retry, error, done }
 
 abstract class TransactionScreenView {
   interval_i.Interval interval = interval_i.Interval.day;
+
+  LoadingState state = LoadingState.loading;
+  Failure failure;
+
+  List<Transaction> transactions = [];
+
+  List<Product> dayTopThree = [];
+  List<Product> weekTopThree = [];
+  List<Product> monthTopThree = [];
+
+  List<Product> dayProductList = [];
+  List<Product> weekProductList = [];
+  List<Product> monthProductList = [];
+
   Widget day;
   Widget week;
   Widget month;
 
-  void onError(BuildContext context);
+  void getProductLists();
 
   void toggleView(interval_i.Interval interval);
+
+  Future getTransactions();
 }
