f597b912e851e44acf0676b66dc5adcb0b5412d7
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-05-23 16:31:09 +0800

    refactor: hotfix null products and comment out unused code

--- frontend/lib/features/transactions/screens/widgets/modes/detailed_view_widget.dart
+++ frontend/lib/features/transactions/screens/widgets/modes/detailed_view_widget.dart
@@ -6,33 +6,43 @@ class DetailedViewWidget extends StatelessWidget {
 
   const DetailedViewWidget({Key key, this.products}) : super(key: key);
 
-  String getPopularVariantData(Product product) {
-    var variants = product.variants;
-    variants.sort((a, b) => b.quantity - a.quantity);
-    return variants.first.variantName;
-  }
+  // String getPopularVariantData(Product product) {
+  //   var variants = product.variants;
 
+  //   print(variants);
+  //   print(variants.first.quantity);
+  //   print('VAR');
+  //   print(product);
+  //   print('PRODUCT');
+  //   variants?.sort((a, b) => b.quantity.compareTo(a.quantity));
+  //   return variants.first.variantName;
+  // }
+
+// TODO: swap w/ CDT [order/widget]
+// TODO: ADD ROW PER VARIANT
   @override
   Widget build(BuildContext context) {
     return DataTable(
       columns: const [
         DataColumn(label: Text("Product")),
         DataColumn(label: Text("Quantity Sold"), numeric: true),
-        DataColumn(label: Text("Most Popular Variant"), numeric: true),
+        // DataColumn(label: Text("Most Popular Variant"), numeric: true),
         DataColumn(label: Text("Total"), numeric: true)
       ],
-      rows: products
-          .map(
-            (e) => DataRow(
-              cells: [
-                DataCell(Text('${e.name}')),
-                DataCell(Text('${e.quantity}')),
-                DataCell(Text(getPopularVariantData(e))),
-                DataCell(Text('${e.quantity * e.variants.last.price}')),
-              ],
-            ),
-          )
-          .toList(),
+      rows: products.isNotEmpty
+          ? products
+              ?.map(
+                (e) => DataRow(
+                  cells: [
+                    DataCell(Text('${e.name}')),
+                    DataCell(Text('${e.quantity}')),
+                    // DataCell(Text(getPopularVariantData(e))),
+                    DataCell(Text('${e.quantity}')), // Dummy
+                  ],
+                ),
+              )
+              ?.toList()
+          : <DataRow>[],
     );
   }
 }
