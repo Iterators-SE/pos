d8f36f8478d173dd0abed575269d8c4c2f408a81
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-05-24 23:33:59 +0800

    refactor: make widget more modifiable

--- frontend/lib/features/orders/screens/widget/custom_data_table.dart
+++ frontend/lib/features/orders/screens/widget/custom_data_table.dart
@@ -7,19 +7,21 @@ class CustomDataTable extends StatefulWidget {
   final Function onPressed;
   final List<DataColumn> columns;
   final List<DataRow> rows;
+  final bool showEdit;
 
-  CustomDataTable({
-    Key key,
-    this.order,
-    this.onPressed,
-    this.columns = const [
-      DataColumn(label: Text("Product")),
-      DataColumn(label: Text("Quantity"), numeric: true),
-      DataColumn(label: Text("Price"), numeric: true),
-      DataColumn(label: Text("Total"), numeric: true)
-    ],
-    this.rows,
-  }) : super(key: key);
+  CustomDataTable(
+      {Key key,
+      this.order,
+      this.onPressed,
+      this.columns = const [
+        DataColumn(label: Text("Product")),
+        DataColumn(label: Text("Quantity"), numeric: true),
+        DataColumn(label: Text("Price"), numeric: true),
+        DataColumn(label: Text("Total"), numeric: true)
+      ],
+      this.rows,
+      this.showEdit = true})
+      : super(key: key);
 
   _CustomDataTableState createState() => _CustomDataTableState();
 }
@@ -29,28 +31,27 @@ class _CustomDataTableState extends State<CustomDataTable> {
   Widget build(BuildContext context) {
     return DataTable(
       columns: widget.columns,
-      rows: widget.rows ?? widget.order.products
-          .map(
-            (e) => DataRow(
-              cells: [
-                DataCell(
-                  Text('${e.productId} [${e.variantName}]'),
+      rows: widget.rows ??
+          widget.order.products
+              .map(
+                (e) => DataRow(
+                  cells: [
+                    DataCell(Text('${e.productId} [${e.variantName}]')),
+                    DataCell(
+                      Text('${e.quantity}'),
+                      showEditIcon: widget.showEdit,
+                      onTap: () => widget.onPressed()(e),
+                    ),
+                    DataCell(
+                      Text('${e.price}'),
+                    ),
+                    DataCell(
+                      Text('${e.quantity * e.price}'),
+                    ),
+                  ],
                 ),
-                DataCell(
-                  Text('${e.quantity}'),
-                  showEditIcon: true,
-                  onTap: () => widget.onPressed()(e),
-                ),
-                DataCell(
-                  Text('${e.price}'),
-                ),
-                DataCell(
-                  Text('${e.quantity * e.price}'),
-                ),
-              ],
-            ),
-          )
-          .toList(),
+              )
+              .toList(),
     );
   }
 }
