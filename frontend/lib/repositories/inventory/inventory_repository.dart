1eee3aafd76512604d5adf63c2cf89666b6d6237
Alanray Baltazar Elangos <ealanray@gmail.com>
2021-05-26 02:31:01 +0800

    refactor: moved variants to inventory repository

--- frontend/lib/repositories/inventory/inventory_repository.dart
+++ frontend/lib/repositories/inventory/inventory_repository.dart
@@ -1,26 +1,26 @@
 import 'package:either_option/either_option.dart';
+import 'package:meta/meta.dart';
+
 import '../../core/error/failure.dart';
+import '../../core/network/network_info.dart';
+import '../../datasources/inventory/inventory_datasource.dart';
+import '../../features/inventory/models/new_product.dart';
+import '../../features/inventory/models/new_variant.dart';
 import '../../models/product.dart';
 
 abstract class IInventoryRepository {
-  Future<Either<Failure, int>> addProduct({
-    String productName,
-    String description,
-    bool isTaxable,
-    String photoLink,
-  });
+  final IInventoryRemoteDataSource remote;
+  final IInventoryLocalDataSource local;
+  final NetworkInfo network;
 
+  const IInventoryRepository(
+      {@required this.remote, @required this.local, @required this.network});
+
+  Future<Either<Failure, Product>> addProduct({NewProduct product});
   Future<Either<Failure, List<Product>>> getProducts();
-  
   Future<Either<Failure, bool>> deleteProduct({int productId});
-
   Future<Either<Failure, Product>> getProductDetails({int productId});
+  Future<Either<Failure, bool>> changeProductDetails({Product product});
 
-  Future<Either<Failure, bool>> changeProductDetails({
-    int productId,
-    String productName,
-    String description,
-    bool isTaxable,
-    String photoLink,
-  });
+  Future<Either<Failure, bool>> addVariant({NewVariant variant});
 }
--- frontend/lib/repositories/inventory/inventory_repository_implementation.dart
+++ frontend/lib/repositories/inventory/inventory_repository_implementation.dart
@@ -1,116 +1,161 @@
 import 'package:either_option/either_option.dart';
 import 'package:graphql/client.dart';
+import 'package:meta/meta.dart';
 
 import '../../core/error/exception.dart';
 import '../../core/error/failure.dart';
-import '../../datasources/inventory/product/inventory_datasource.dart';
+import '../../core/network/network_info.dart';
+import '../../datasources/inventory/inventory_local_datasource.dart';
+import '../../datasources/inventory/inventory_remote_datasource.dart';
+import '../../features/inventory/models/new_product.dart';
+import '../../features/inventory/models/new_variant.dart';
 import '../../models/product.dart';
 import 'inventory_repository.dart';
 
 class InventoryRepository implements IInventoryRepository {
-  final IInventoryDataSource remote;
+  final InventoryRemoteDataSource remote;
+  final InventoryLocalDataSource local;
+  final NetworkInfo network;
 
-  InventoryRepository({this.remote});
+  InventoryRepository(
+      {@required this.remote, @required this.local, @required this.network});
 
   @override
-  Future<Either<Failure, int>> addProduct({
-    String productName,
-    String description,
-    bool isTaxable,
-    String photoLink,
-  }) async {
-    try {
-      final data = await remote.addProduct(
-          productName: productName,
-          description: description,
-          isTaxable: isTaxable,
-          photoLink: photoLink);
-      return Right(data);
-    } on OperationException catch (e) {
-      return Left(OperationFailure(e.graphqlErrors.first.message));
-    } on NoResultsFoundException {
-      return Left(NoResultsFoundFailure());
-    } on Exception {
-      return Left(ServerFailure());
-    } catch (e) {
-      return Left(UnhandledFailure());
+  Future<Either<Failure, Product>> addProduct({NewProduct product}) async {
+    if (await network.isConnected()) {
+      try {
+        print("this thing ran");
+        final data = await remote.addProduct(product: product);
+        return Right(data);
+      } on OperationException catch (e) {
+        return Left(OperationFailure(e.graphqlErrors.first.message));
+      } on NoResultsFoundException {
+        return Left(NoResultsFoundFailure());
+      } on Exception {
+        return Left(ServerFailure());
+      } catch (e) {
+        return Left(UnhandledFailure());
+      }
     }
+
+    return Left(UnhandledFailure());
   }
 
   @override
   Future<Either<Failure, bool>> deleteProduct({int productId}) async {
-    try {
-      final data = await remote.deleteProduct(productId: productId);
-      return Right(data);
-    } on OperationException catch (e) {
-      return Left(OperationFailure(e.graphqlErrors.first.message));
-    } on NoResultsFoundException {
-      return Left(NoResultsFoundFailure());
-    } on Exception {
-      return Left(ServerFailure());
-    } catch (e) {
-      return Left(UnhandledFailure());
+    if (await network.isConnected()) {
+      try {
+        final data = await remote.deleteProduct(productId: productId);
+        return Right(data);
+      } on OperationException catch (e) {
+        return Left(OperationFailure(e.graphqlErrors.first.message));
+      } on NoResultsFoundException {
+        return Left(NoResultsFoundFailure());
+      } on Exception {
+        return Left(ServerFailure());
+      } catch (e) {
+        return Left(UnhandledFailure());
+      }
     }
+
+    return Left(UnhandledFailure());
   }
 
   @override
   Future<Either<Failure, List<Product>>> getProducts() async {
-    try {
-      final data = await remote.getProducts();
-      print(data);
-      return Right(data);
-    } on OperationException catch (e) {
-      return Left(OperationFailure(e.graphqlErrors.first.message));
-    } on NoResultsFoundException {
-      return Left(NoResultsFoundFailure());
-    } on Exception {
-      return Left(ServerFailure());
-    } catch (e) {
-      return Left(UnhandledFailure());
+    if (await network.isConnected()) {
+      try {
+        final data = await remote.getProducts();
+        print(await data);
+        await local.cacheProducts(data);
+        return Right(data);
+      } on OperationException catch (e) {
+        return Left(OperationFailure(e.graphqlErrors.first.message));
+      } on NoResultsFoundException {
+        return Left(NoResultsFoundFailure());
+      } on Exception {
+        return Left(ServerFailure());
+      } catch (e) {
+        return Left(UnhandledFailure());
+      }
+    } else {
+      try {
+        final data = await local.getProducts();
+        return Right(data);
+      } on CacheException {
+        return Left(CacheFailure());
+      } catch (e) {
+        return Left(UnhandledFailure());
+      }
     }
   }
 
   @override
   Future<Either<Failure, Product>> getProductDetails({int productId}) async {
-    try {
-      final data = await remote.getProductDetails(productId: productId);
-      return Right(data);
-    } on OperationException catch (e) {
-      return Left(OperationFailure(e.graphqlErrors.first.message));
-    } on NoResultsFoundException {
-      return Left(NoResultsFoundFailure());
-    } on Exception {
-      return Left(ServerFailure());
-    } catch (e) {
-      return Left(UnhandledFailure());
+    if (await network.isConnected()) {
+      try {
+        final data = await remote.getProductDetails(productId: productId);
+        await local.cacheProduct(data);
+        return Right(data);
+      } on OperationException catch (e) {
+        return Left(OperationFailure(e.graphqlErrors.first.message));
+      } on NoResultsFoundException {
+        return Left(NoResultsFoundFailure());
+      } on Exception {
+        return Left(ServerFailure());
+      } catch (e) {
+        return Left(UnhandledFailure());
+      }
+    } else {
+      try {
+        final data = await local.getProductDetails(productId: productId);
+        return Right(data);
+      } on CacheException {
+        return Left(CacheFailure());
+      } catch (e) {
+        return Left(UnhandledFailure());
+      }
     }
   }
 
   @override
   @override
-  Future<Either<Failure, bool>> changeProductDetails({
-    int productId,
-    String productName,
-    String description,
-    bool isTaxable,
-    String photoLink,
-  }) async {
-    try {
-      final data = await remote.changeProductDetails(
-          productId: productId,
-          description: description,
-          productName: productName,
-          isTaxable: isTaxable,
-          photoLink: photoLink);
-      return Right(data);
-    } on OperationException catch (e) {
-      return Left(OperationFailure(e.graphqlErrors.first.message));
-    } on NoResultsFoundException {
-      return Left(NoResultsFoundFailure());
-    } on Exception {
-      return Left(ServerFailure());
-    } catch (e) {
-      return Left(UnhandledFailure());
+  Future<Either<Failure, bool>> changeProductDetails({Product product}) async {
+    if(await network.isConnected()){
+      try {
+        final data = await remote.changeProductDetails(product: product);
+        return Right(data);
+      } on OperationException catch (e) {
+        return Left(OperationFailure(e.graphqlErrors.first.message));
+      } on NoResultsFoundException {
+        return Left(NoResultsFoundFailure());
+      } on Exception {
+        return Left(ServerFailure());
+      } catch (e) {
+        return Left(UnhandledFailure());
+      }
     }
+
+    return Left(UnhandledFailure());
+  }
+
+  @override
+  Future<Either<Failure, bool>> addVariant({NewVariant variant}) async {
+    if(await network.isConnected()){  
+      try {
+        final data = await remote.addVariant(variant: variant);
+        return Right(data);
+      } on OperationException catch (e) {
+        return Left(OperationFailure(e.graphqlErrors.first.message));
+      } on NoResultsFoundException {
+        return Left(NoResultsFoundFailure());
+      } on Exception {
+        return Left(ServerFailure());
+      } catch (e) {
+        return Left(UnhandledFailure());
+      }
+    } 
+
+    return Left(UnhandledFailure());
   }
 }
