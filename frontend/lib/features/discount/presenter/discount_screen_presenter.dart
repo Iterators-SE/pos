dd0f5f99751a81dbc03170199f9764109e50501d
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-05-25 09:52:07 +0800

    feat: add initial discount presenter

---
+++ frontend/lib/features/discount/presenter/discount_screen_presenter.dart
@@ -0,0 +1,13 @@
+// import 'package:flutter/material.dart';
+
+import '../../../core/presenters/base_presenter.dart';
+import '../view/discount_screen_view.dart';
+
+
+class DiscountScreenPresenter extends BasePresenter<DiscountScreenView> {
+  // Widget getMessage() {
+  //   checkViewAttached();
+  //   return isViewAttached  && getView().hasProducts ? 
+  // getView().body : getView().emptyOrder;
+  // }
+}
