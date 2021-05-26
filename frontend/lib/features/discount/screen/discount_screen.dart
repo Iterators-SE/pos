1f83f6fd4474333fce4a0a88752335b0ada5775a
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-05-26 21:08:37 +0800

    feat: add initial discount screen

--- frontend/lib/features/discount/screen/discount_screen.dart
+++ frontend/lib/features/discount/screen/discount_screen.dart
@@ -1,16 +1,16 @@
+import 'package:either_option/either_option.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/core/error/failure.dart';
 import 'package:provider/provider.dart';
 
 import '../../../core/state/app_state.dart';
 import '../../../models/discounts.dart';
-import '../../../repositories/discount/discount_repository.dart';
-import '../add/discount_add_generic.dart';
-import '../details/discount_details.dart';
+import '../../../repositories/discount/discount_repository_implementation.dart';
 import '../presenter/discount_screen_presenter.dart';
-import '../view/add_discount_screen_view.dart';
 import '../view/discount_screen_view.dart';
-import '../view/edit_discount_screen_view.dart';
-import 'widgets/title.dart';
+import 'generic_discount_screen.dart';
+import 'widgets/custom_discount_fab.dart';
+import 'widgets/page/discount_page.dart';
 
 class DiscountScreen extends StatefulWidget {
   @override
@@ -22,27 +22,34 @@ class _DiscountScreenState extends State<DiscountScreen>
   DiscountScreenPresenter _presenter;
 
   @override
-  AppState state;
+  AppState state = AppState.loading;
+
+  @override
+  Widget body;
 
   @override
   List<Discount> discounts = [];
-
-  @override
-  AddDiscountScreenView addDiscountScreenView;
-
-  @override
-  EditDiscountScreenView editDiscountScreenView;
 
   @override
   void initState() {
     _presenter = DiscountScreenPresenter();
     _presenter.view = this;
 
-    state = AppState.loading;
-    // addDiscountScreenView = AddDiscountScreen();
-    // editDiscountScreenView = EditDiscountScreen();
+    getDiscounts().then((value) {
+      var result = value.fold((failure) => failure, (list) => list);
 
-    getDiscounts().then((value) => {discounts = value});
+      if (value.isRight) {
+        setState(() {
+          discounts = result;
+          state = AppState.done;
+          body = DiscountPage(discounts: discounts);
+        });
+      } else {
+        setState(() {
+          state = AppState.error;
+        });
+      }
+    });
 
     super.initState();
   }
@@ -51,48 +58,31 @@ class _DiscountScreenState extends State<DiscountScreen>
   void onError() {}
 
   @override
-  String formatTime(String time) {
-    var timeOfDay = time.split(' ')[0];
-    var amOrPM = time.split(' ')[1];
+  Future<Either<Failure, List<Discount>>> getDiscounts() async {
+    // var discounts =
+    //     await Provider.of<DiscountRepository>(context, listen: false)
+    //         .getDiscounts();
 
-    if (amOrPM == 'PM') {
-      var hour = timeOfDay.split(':')[0];
-      var minutes = timeOfDay.split(':')[1];
-      var newHours = int.parse(hour) + 12;
-      return [newHours.toString(), minutes].join(':');
-    }
+    // return discounts;
 
-    return timeOfDay;
-  }
 
-  // ignore: missing_return
-  Future<List<Discount>> getDiscounts() async {
-    var discounts =
-        await Provider.of<DiscountRepository>(context, listen: false)
-            .getDiscounts();
+    // MOCK
+    var fake = await [
+      Discount(
+        id: 1,
+        percentage: 20,
+        products: [1, 2, 3],
+        description: "Senior Citizen",
+      ),
+      Discount(
+        id: 2,
+        percentage: 15,
+        products: [1, 3],
+        description: "PWD",
+      )
+    ];
 
-    var result = discounts.fold((failure) => failure, (list) => list);
-
-    if (discounts.isRight) {
-      setState(() {
-        this.discounts = result;
-        state = AppState.loaded;
-      });
-    } else {
-      setState(() {
-        state = AppState.error;
-      });
-    }
-    // var prefs = await SharedPreferences.getInstance();
-    // var token = prefs.getString('POS_TOKEN');
-    // GraphQLConfiguration.setToken(token);
-    // var query = MutationQuery();
-    // var client = GraphQLConfiguration().clientToQuery();
-
-    // var result = await client.query(
-    //   QueryOptions(document: gql(query.getDiscounts())),
-    // );
-    // return result.data['getDiscounts'];
+    return Right(fake);
   }
 
   @override
@@ -104,66 +94,17 @@ class _DiscountScreenState extends State<DiscountScreen>
           style: TextStyle(fontFamily: "Montserrat Bold"),
         ),
       ),
-      floatingActionButton: FloatingActionButton.extended(
-        icon: Icon(Icons.add),
-        label: Text(
-          "ADD",
-          style: TextStyle(fontFamily: "Montserrat Bold"),
+      floatingActionButton: CustomDiscountFAB(
+        onPressed: () => Navigator.push(
+          context,
+          MaterialPageRoute(
+            builder: (context) => GenericDiscountScreen(),
+          ),
         ),
-        onPressed: () {
-          Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => AddGenericDiscount(),
-            ),
-          );
-        },
+        label: "ADD",
+        icon: Icons.add,
       ),
-      body: FutureBuilder(
-          future: getDiscounts(),
-          builder: (context, snapshot) {
-            if (snapshot.data != null) {
-              return Padding(
-                padding: const EdgeInsets.only(left: 50, right: 50),
-                child: GridView.builder(
-                    itemCount: snapshot.data.length,
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: MediaQuery.of(context).orientation ==
-                              Orientation.landscape
-                          ? 3
-                          : 2,
-                      crossAxisSpacing: 8,
-                      mainAxisSpacing: 8,
-                    ),
-                    itemBuilder: (context, index) {
-                      return Padding(
-                        padding: EdgeInsets.all(20),
-                        child: ElevatedButton(
-                          style: ElevatedButton.styleFrom(
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(30.0),
-                            ),
-                          ),
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(
-                                builder: (context) => DiscountDetails(
-                                  id: int.parse(snapshot.data[index]['id']),
-                                ),
-                              ),
-                            );
-                          },
-                          child: discountTitles(
-                            snapshot.data[index]['description'],
-                          ),
-                        ),
-                      );
-                    }),
-              );
-            }
-            return Text('No discounts found');
-          }),
+      body: _presenter.body(),
     );
   }
 }
