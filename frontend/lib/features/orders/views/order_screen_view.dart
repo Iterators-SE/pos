4820b1af2f590eaebe476885e7bce0a964528121
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-05-27 17:18:46 +0800

    refactor: modify order screen view

--- frontend/lib/features/orders/views/order_screen_view.dart
+++ frontend/lib/features/orders/views/order_screen_view.dart
@@ -1,20 +1,31 @@
+import 'package:either_option/either_option.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/core/state/app_state.dart';
 
+import '../../../core/error/failure.dart';
+import '../../../models/product.dart';
 import '../../../models/product_variant.dart';
 import '../models/order.dart';
 
 abstract class OrderScreenView {
   Order order;
+  AppState state;
+
+  List<Product> allProducts;
 
   bool hasProducts;
-  Widget body;
 
-  void onError();
+  Widget body;
+  Failure failure;
+
 
   void addProduct(ProductVariant product);
 
   Function addDiscount();
-  
+
+  Future<Either<Failure, List<Product>>> getProducts();
+
   Function cancelOrder();
+
   Function processOrder();
 }
