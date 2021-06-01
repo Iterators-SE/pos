5a416c84a017b103adf1cd28f9da7888da64c1be
zrtheya <zrtheyatuares@yahoo.com>
2021-06-01 20:53:06 +0800

    feat: add profile user

---
+++ frontend/lib/features/profile/page/profile_page.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/core/themes/config.dart';
+import 'package:frontend/features/profile/models/user.dart';
+import 'package:frontend/features/profile/screens/widgets/appbar_widget.dart';
+
+import 'package:frontend/features/profile/screens/widgets/profile_widget.dart';
+import 'package:frontend/features/profile/utils/user_preferences.dart';
+
+import 'edit_profile_page.dart';
+
+class ProfilePage extends StatefulWidget {
+  @override
+  _ProfilePageState createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final user = UserPreferences.myUser;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: buildAppbar(context),
+      body: ListView(
+        physics: BouncingScrollPhysics(),
+        children: [
+          SizedBox(
+            height: 30,
+          ),
+          ProfileWidget(
+              imagePath: user.imagePath,
+              onClicked: () {
+                print("clicked");
+                // Navigator.of(context).push(
+                //   MaterialPageRoute(builder: (context) => EditProfilePage()),
+              }),
+          SizedBox(
+            height: 30,
+          ),
+          buildName(user),
+          SizedBox(
+            height: 30,
+          ),
+          buildMessage(user),
+        ],
+      ),
+    );
+  }
+
+  Widget buildName(User user) => Column(
+        children: [
+          Text(
+            user.name,
+            style: TextStyle(fontWeight: FontWeight.bold, fontSize: 28),
+          ),
+          SizedBox(height: 5),
+          Text(
+            user.email,
+            style: TextStyle(color: Colors.grey),
+          ),
+          SizedBox(height: 5),
+          Text(
+            user.address,
+            style: TextStyle(fontSize: 18),
+          )
+        ],
+      );
+
+  Widget buildMessage(User user) => Stack(children: [
+        Container(
+          height: 120,
+          margin: EdgeInsets.all(10),
+          child: InputDecorator(
+            decoration: InputDecoration(
+              labelText: 'Message',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(10.0),
+              ),
+            ),
+            // child: Text(user.message),
+          ),
+        ),
+        Positioned(top: 25, left: 30, child: Text(user.message))
+      ]);
+}
