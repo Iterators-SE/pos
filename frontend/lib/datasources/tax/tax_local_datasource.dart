da606f9222ee90352ac9795ea3ff4344441d813f
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-06-09 00:40:27 +0800

    fix: fix type annotation in taxes local data source

--- frontend/lib/datasources/tax/tax_local_datasource.dart
+++ frontend/lib/datasources/tax/tax_local_datasource.dart
@@ -6,8 +6,8 @@ class TaxLocalDataSource implements ITaxLocalDataSource {
   TaxLocalDataSource({this.local});
   @override
   Future<void> cacheTaxes(dynamic data) async{
-      final List<Tax> taxes = await data.map((tax) {
-        return Tax(
+      final List<Taxe> taxes = await data.map((tax) {
+        return Taxe(
           id: tax['id'],
           name: tax['name'],
           isSelected: tax['isSelected'],
@@ -18,17 +18,17 @@ class TaxLocalDataSource implements ITaxLocalDataSource {
     }
   
     @override
-    Future<Tax> getSelectedTax() async{
+    Future<Taxe> getSelectedTax() async{
       return await local.getSelectedTax();
     }
   
     @override
-    Future<Tax> getTaxDetails(int taxId)async {
+    Future<Taxe> getTaxDetails(int taxId)async {
       return await local.getTaxDetails(taxId);
   }
 
   @override
-  Future<List<Tax>> getTaxes() async{
+  Future<List<Taxe>> getTaxes() async{
     return await local.getTaxes();
   }
   
