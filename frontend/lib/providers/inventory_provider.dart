7d5457f6eef806f3f616982fea1c2b7d381c39c8
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-06-09 00:45:00 +0800

    chore: address linter errors

--- frontend/lib/providers/inventory_provider.dart
+++ frontend/lib/providers/inventory_provider.dart
@@ -1,33 +1,14 @@
 import 'package:either_option/either_option.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:graphql_flutter/graphql_flutter.dart';
 import 'package:provider/provider.dart';
 
 import '../core/error/failure.dart';
 import '../features/inventory/models/new_product.dart';
 import '../models/product.dart';
 import '../repositories/inventory/inventory_repository_implementation.dart';
-import 'user_provider.dart';
 
 class InventoryProvider extends ChangeNotifier {
-
-  // final InventoryRepository inventoryRepository;
-
-  // InventoryProvider({
-  //   this.inventoryRepository
-  // });
-
-  // void setAuthenticated(BuildContext context) async {
-  //       var token = Provider.of<UserProvider>(context, listen: false).token;
-
-  //   inventoryRepository.remote.client = GraphQLClient(
-  //     cache: GraphQLCache(),
-  //     link: AuthLink(getToken: () => 'Bearer $token')
-  //         .concat(HttpLink('http://iterators-pos.herokuapp.com/graphql')),
-  //   );
-  // }
-
   Future<Either<Failure, bool>> addProduct(
       BuildContext context, NewProduct product) async {
     var provider =
