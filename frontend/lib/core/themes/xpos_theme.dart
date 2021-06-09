91de34e955707cad084e70e05d1493c226bcb165
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-06-10 01:47:05 +0800

    refactor: update theme

--- frontend/lib/core/themes/xpos_theme.dart
+++ frontend/lib/core/themes/xpos_theme.dart
@@ -16,26 +16,13 @@ class XPosTheme extends ChangeNotifier {
   static ThemeData get lightTheme {
     return ThemeData(
       primarySwatch: xposGreen,
-      // primaryColor: Color(primaryColor),
       scaffoldBackgroundColor: Color(backgroundColor),
       fontFamily: 'Montserrat',
       buttonTheme: ButtonThemeData(
-        buttonColor: xposGreen[500],
+        buttonColor: xposGreen[50],
       ),
       floatingActionButtonTheme: FloatingActionButtonThemeData(
-        backgroundColor: xposGreen[500],
-      ),
-    );
-  }
-
-  // TODO: change color palette for dark mode
-  static ThemeData get darkTheme {
-    return ThemeData(
-      primaryColor: Color(primaryColor),
-      scaffoldBackgroundColor: Color(backgroundColor),
-      fontFamily: 'Montserrat',
-      buttonTheme: ButtonThemeData(
-        buttonColor: Color(primaryColor),
+        backgroundColor: xposGreen[50],
       ),
     );
   }
