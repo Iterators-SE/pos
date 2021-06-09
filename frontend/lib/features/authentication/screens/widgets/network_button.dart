08ec0dd65be67986962b0da86ad37b36819619d4
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-06-10 00:23:57 +0800

    feat: create newtork button widget

---
+++ frontend/lib/features/authentication/screens/widgets/network_button.dart
@@ -0,0 +1,37 @@
+import 'package:flutter/material.dart';
+import 'package:meta/meta.dart';
+
+import '../../../../core/themes/config.dart';
+
+class NetworkButton extends StatelessWidget {
+  final Function onPressed;
+  final String text;
+  final String buttonKey;
+
+  const NetworkButton({
+    Key key,
+    @required this.onPressed,
+    @required this.text,
+    @required this.buttonKey,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 40,
+      width: 80,
+      decoration: BoxDecoration(
+        color: xposGreen[50],
+        borderRadius: BorderRadius.circular(10),
+      ),
+      child: MaterialButton(
+        key: Key(buttonKey),
+        onPressed: onPressed,
+        child: Text(
+          text,
+          style: TextStyle(color: Colors.white),
+        ),
+      ),
+    );
+  }
+}
