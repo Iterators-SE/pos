bd78240c372f1c2e7b4fb4274e7cf472651787c1
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-06-14 17:42:47 +0800

    refactor: provide fallback to select models

--- frontend/lib/models/transaction.dart
+++ frontend/lib/models/transaction.dart
@@ -17,15 +17,15 @@ class Transaction {
   });
 
   factory Transaction.fromJson(Map<String, dynamic> json) => Transaction(
-        id: json["id"],
+        id: int.tryParse(json["id"]) ?? json["id"],
         orders: json["orders"].map((order) => Order.fromJson(order)).toList(),
-        createdAt: json["createdAt"],
+        createdAt: DateTime.tryParse(json["createdAt"]) ?? json["createdAt"],
       );
 
   Map<String, dynamic> toJson() => {
         "id": id,
         "orders": orders,
-        "createdAt": createdAt,
+        "createdAt": createdAt.toString(),
       };
 
   @override
