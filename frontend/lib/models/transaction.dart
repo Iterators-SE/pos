a27773945c138d8ca4e73bc0eddda80ce9137331
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-06-15 18:49:31 +0800

    feat: add link to transaction model in frontend

--- frontend/lib/models/transaction.dart
+++ frontend/lib/models/transaction.dart
@@ -6,6 +6,7 @@ import 'tax.dart';
 class Transaction {
   final int id;
   final List<Order> orders;
+  final String link;
   Tax tax = Tax(percentage: 0);
   final DateTime createdAt;
 
@@ -13,27 +14,21 @@ class Transaction {
     @required this.id,
     @required this.orders,
     @required this.createdAt,
+    this.link,
     this.tax,
   });
 
-  // factory Transaction.fromJson(Map<String, dynamic> json) => Transaction(
-  //       id: int.parse(json["id"]),
-  //       orders: json["orders"]
-  //                .map((order) => Order.fromJson(order)).toList(),
-  //       createdAt: DateTime.tryParse(json["createdAt"]) ?? json["createdAt"],
-  //     );
-
   factory Transaction.fromJson(Map<String, dynamic> json) {
-    
-    var orders  = <Order>[];
+    var orders = <Order>[];
 
-    for(var i = 0; i < json['orders'].length; i++){
+    for (var i = 0; i < json['orders'].length; i++) {
       orders.add(Order.fromJson(json['orders'][i]));
     }
-    
+
     return Transaction(
       id: int.parse(json["id"]),
       orders: orders,
+      link: json["link"],
       createdAt: DateTime.tryParse(json["createdAt"]) ?? json["createdAt"],
     );
   }
