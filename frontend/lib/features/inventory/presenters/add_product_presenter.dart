326cf45ec45e61b134830615c8a15f285486dbbe
Alanray Elangos <ealanray@gmail.com>
2021-06-16 02:52:22 +0800

    feat: removed complete message and button and replaced with snackbar

--- frontend/lib/features/inventory/presenters/add_product_presenter.dart
+++ frontend/lib/features/inventory/presenters/add_product_presenter.dart
@@ -14,34 +14,41 @@ class AddProductScreenPresenter extends BasePresenter<AddProductScreenView> {
     } else if (isViewAttached && getView().state == AppState.done) {
       return getView().body;
     } else if (isViewAttached && getView().state == AppState.successful) {
-      return Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            // Center(
-            //   child: Image(
-            //   image: AssetImage(
-            //   'images/blue_added.png',),
-            //     width: 150,
-            //     height: 160,
-            //   )
-            // ), 
-            Align(
-              alignment: Alignment.center, 
-              child: Text("Product added to inventory"),
-            ),
-            SizedBox(height: 30),
-            ElevatedButton(
-                onPressed: () async {
-                  Navigator.pop(context);
-                },
-                child: Text("Okay"))
-          ],
-        ),
-      );
+
+      Future.delayed(Duration(milliseconds: 100), () {
+        Navigator.pop(context, AppState.successful);
+      });
+
+      // return Center(
+      //   child: Column(
+      //     mainAxisAlignment: MainAxisAlignment.center,
+      //     crossAxisAlignment: CrossAxisAlignment.center,
+      //     children: [
+      //       // Center(
+      //       //   child: Image(
+      //       //   image: AssetImage(
+      //       //   'images/blue_added.png',),
+      //       //     width: 150,
+      //       //     height: 160,
+      //       //   )
+      //       // ), 
+      //       Align(
+      //         alignment: Alignment.center, 
+      //         child: Text("Product added to inventory"),
+      //       ),
+      //       SizedBox(height: 30),
+      //       ElevatedButton(
+      //           onPressed: () async {
+      //             Navigator.pop(context);
+      //           },
+      //           child: Text("Okay"))
+      //     ],
+      //   ),
+      // );
     } else {
-      return Center(child: Text("Oh no, there's a problem!"));
+      Future.delayed(Duration(seconds: 1), () {
+        Navigator.pop(context, AppState.error);
+      });
     }
   }
 }
