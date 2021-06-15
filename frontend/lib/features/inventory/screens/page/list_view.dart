57c17306f181d620d5028c3c36060a2b9f31d155
Majpotatooo <majella.jereza.phowzzz@gmail.com>
2021-06-15 21:35:51 +0800

    style: search list

--- frontend/lib/features/inventory/screens/page/list_view.dart
+++ frontend/lib/features/inventory/screens/page/list_view.dart
@@ -34,9 +34,14 @@ class _ProductListPageState extends State<ProductListPage> {
         child: ListView.builder(
             itemCount: filteredProducts.length,
             itemBuilder: (contex, index) {
-              return ProductListTile(
+              return Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+            ),
+            elevation: 4,
+            child: ProductListTile(
                   product: filteredProducts[index],
-                  functionOnTap: widget.functionOnTap);
+                  functionOnTap: widget.functionOnTap));
             }),
       );
     }
@@ -44,9 +49,15 @@ class _ProductListPageState extends State<ProductListPage> {
       child: ListView.builder(
           itemCount: widget.products.length,
           itemBuilder: (contex, index) {
-            return ProductListTile(
+            return Card(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(10.0),
+            ),
+            elevation: 4,
+            child:ProductListTile(
                 product: widget.products[index],
-                functionOnTap: widget.functionOnTap);
+                functionOnTap: widget.functionOnTap)
+            );
           }),
     );
   }
