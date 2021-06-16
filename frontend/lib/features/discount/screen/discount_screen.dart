0663b320764686eb4e3543538e4e733e8d49920d
Anike Nicole Dorgu <naddorgu@gmail.com>
2021-06-16 10:38:29 +0800

    feat: add real data

--- frontend/lib/features/discount/screen/discount_screen.dart
+++ frontend/lib/features/discount/screen/discount_screen.dart
@@ -1,14 +1,13 @@
 import 'package:either_option/either_option.dart';
 import 'package:flutter/material.dart';
-
-// import 'package:frontend/repositories/inventory/inventory_repository_implementation.dart';
-// import 'package:provider/provider.dart';
+import 'package:provider/provider.dart';
 
 import '../../../core/error/failure.dart';
 import '../../../core/state/app_state.dart';
 import '../../../models/discounts.dart';
 import '../../../models/product.dart';
-// import '../../../repositories/discount/discount_repository_implementation.dart';
+import '../../../repositories/discount/discount_repository_implementation.dart';
+import '../../../repositories/inventory/inventory_repository_implementation.dart';
 import '../presenter/discount_screen_presenter.dart';
 import '../view/discount_screen_view.dart';
 import 'generic_discount_screen.dart';
@@ -73,57 +72,20 @@ class _DiscountScreenState extends State<DiscountScreen>
 
   @override
   Future<Either<Failure, List<Product>>> getProducts() async {
-    // var products =
-    //     await Provider.of<InventoryRepository>(context, listen: false)
-    //         .getProducts();
+    var products =
+        await Provider.of<InventoryRepository>(context, listen: false)
+            .getProducts();
 
-    // return products;
-    var fake = await [
-      Product(
-        id: 1,
-        name: 'Fake',
-        description: 'Frapucapucino',
-      ),
-      Product(
-        id: 2,
-        name: 'Faker',
-        description: "Kape",
-      ),
-      Product(
-        id: 3,
-        name: 'Fake Kopi',
-        description: "Kopi",
-      ),
-    ];
-
-    return Right(fake);
+    return products;
   }
 
   @override
   Future<Either<Failure, List<Discount>>> getDiscounts() async {
-    // var discounts =
-    //     await Provider.of<DiscountRepository>(context, listen: false)
-    //         .getDiscounts();
+    var discounts =
+        await Provider.of<DiscountRepository>(context, listen: false)
+            .getDiscounts();
 
-    // return discounts;
-
-    // MOCK
-    var fake = await [
-      Discount(
-        id: 1,
-        percentage: 20,
-        products: [1, 2, 3],
-        description: "Senior Citizen",
-      ),
-      Discount(
-        id: 2,
-        percentage: 15,
-        products: [1, 3],
-        description: "PWD",
-      )
-    ];
-
-    return Right(fake);
+    return discounts;
   }
 
   @override
